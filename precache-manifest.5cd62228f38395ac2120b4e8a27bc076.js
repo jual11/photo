6e2250cbbf2e043370fe406bfee545ee9478effc
Alari Jurtom <alarijurtom@gmail.com>
2020-01-10 19:02:06 +0200

    Updates

---
+++ precache-manifest.5cd62228f38395ac2120b4e8a27bc076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7045301b2b4b6998828c",
+    "url": "/photo/static/js/runtime~main.4809135d.js"
+  },
+  {
+    "revision": "a5254d16aa8fc144d8c4",
+    "url": "/photo/static/js/main.cd80f7e3.chunk.js"
+  },
+  {
+    "revision": "3c38469efb76f20c66f2",
+    "url": "/photo/static/js/2.03a30d3f.chunk.js"
+  },
+  {
+    "revision": "a5254d16aa8fc144d8c4",
+    "url": "/photo/static/css/main.cc426b74.chunk.css"
+  },
+  {
+    "revision": "0313b416292aab867fc96485a2d46438",
+    "url": "/photo/index.html"
+  }
+];
